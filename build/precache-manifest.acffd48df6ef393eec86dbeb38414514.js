d25a5859cbb797918e068cfe0cd70142bca72939
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-10-22 13:19:30 +0200

    build folder anabled

---
+++ build/precache-manifest.acffd48df6ef393eec86dbeb38414514.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67adc487634d7a75376e576fc25c0be9",
+    "url": "/portfolio-mk3/index.html"
+  },
+  {
+    "revision": "b6aef30940aa39cd5950",
+    "url": "/portfolio-mk3/static/css/main.c303116a.chunk.css"
+  },
+  {
+    "revision": "8fd3b3ba98725545e4e6",
+    "url": "/portfolio-mk3/static/js/2.47aa74be.chunk.js"
+  },
+  {
+    "revision": "b6aef30940aa39cd5950",
+    "url": "/portfolio-mk3/static/js/main.8af7eb66.chunk.js"
+  },
+  {
+    "revision": "57bfdc41466e3927475b",
+    "url": "/portfolio-mk3/static/js/runtime-main.eab1ac9a.js"
+  },
+  {
+    "revision": "f44435d58a5c488964c6979583dc25a3",
+    "url": "/portfolio-mk3/static/media/contact.f44435d5.jpg"
+  },
+  {
+    "revision": "62214d5c679edfd3a4ad3fdc0e4055e8",
+    "url": "/portfolio-mk3/static/media/css3Icon.62214d5c.svg"
+  },
+  {
+    "revision": "1baf7687f978d8cee561f1e78873c185",
+    "url": "/portfolio-mk3/static/media/githubRound.1baf7687.svg"
+  },
+  {
+    "revision": "e6990cd71a262e8ebc7e102181da5bcc",
+    "url": "/portfolio-mk3/static/media/graphQlIcon.e6990cd7.svg"
+  },
+  {
+    "revision": "55b8b4506c22f9141d326251ac1cef3a",
+    "url": "/portfolio-mk3/static/media/home.55b8b450.jpg"
+  },
+  {
+    "revision": "d22bcaa3094f7bfde5803c416c17b677",
+    "url": "/portfolio-mk3/static/media/html5Icon.d22bcaa3.svg"
+  },
+  {
+    "revision": "8b748e55ec6a70800f0913e26d53ec19",
+    "url": "/portfolio-mk3/static/media/interests.8b748e55.jpg"
+  },
+  {
+    "revision": "bbc5738c1aefe99a0df9e0fe479ba1a0",
+    "url": "/portfolio-mk3/static/media/jsIcon.bbc5738c.svg"
+  },
+  {
+    "revision": "e5289347c3de259559fcd065ca39dfb4",
+    "url": "/portfolio-mk3/static/media/linkedInRound.e5289347.svg"
+  },
+  {
+    "revision": "e470d90543b3b5cbc7d56d5a3fde878b",
+    "url": "/portfolio-mk3/static/media/mailRound.e470d905.svg"
+  },
+  {
+    "revision": "9885c6b8a5c0526388d452a9e95e1f9e",
+    "url": "/portfolio-mk3/static/media/npmIcon.9885c6b8.svg"
+  },
+  {
+    "revision": "5b6fb53524b0d7a0b0db0f645a440d57",
+    "url": "/portfolio-mk3/static/media/pg.5b6fb535.jpg"
+  },
+  {
+    "revision": "4b74176dd4bd8486b19824e9dfe6823c",
+    "url": "/portfolio-mk3/static/media/projects.4b74176d.jpg"
+  },
+  {
+    "revision": "c72def3d926df926d128930704cf8972",
+    "url": "/portfolio-mk3/static/media/reactIcon.c72def3d.svg"
+  },
+  {
+    "revision": "46f296b26f8ebceb01998c5d70012daf",
+    "url": "/portfolio-mk3/static/media/reduxIcon.46f296b2.svg"
+  },
+  {
+    "revision": "639cffb9c0905f78c298d885fdcd1de9",
+    "url": "/portfolio-mk3/static/media/sassIcon.639cffb9.svg"
+  },
+  {
+    "revision": "a9381b05aa828f68806d398f9df8fdea",
+    "url": "/portfolio-mk3/static/media/typescriptIcon.a9381b05.svg"
+  }
+]);
