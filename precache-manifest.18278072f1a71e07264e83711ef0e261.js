56589330561fd873ebc48b472130c415b442b357
Darek Sikorra <dariusz.sikorra@gmail.com>
2020-01-19 16:13:22 +0100

    Updates

--- precache-manifest.18278072f1a71e07264e83711ef0e261.js
+++ precache-manifest.18278072f1a71e07264e83711ef0e261.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd141a6af3eb6981d3a9a70229a3a9a9",
+    "revision": "4d6b84b026cb9c73238e804d843beb66",
     "url": "/portfolio-mk3/index.html"
   },
   {
-    "revision": "4a6a72603b693da3098f",
+    "revision": "6341b76ce0ffc70b68cd",
     "url": "/portfolio-mk3/static/css/main.e834ae10.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-mk3/static/js/2.a2108c52.chunk.js.LICENSE"
   },
   {
-    "revision": "4a6a72603b693da3098f",
-    "url": "/portfolio-mk3/static/js/main.29d4a74d.chunk.js"
+    "revision": "6341b76ce0ffc70b68cd",
+    "url": "/portfolio-mk3/static/js/main.3afe4ff1.chunk.js"
   },
   {
     "revision": "65163f4ac1a14995e14b",
